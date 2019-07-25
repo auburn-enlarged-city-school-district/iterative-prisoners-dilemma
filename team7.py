43fc8d5a383d8a203ebb826ad3e0caba8203ad08
bwwatson24 <53262899+bwwatson24@users.noreply.github.com>
2019-07-25 11:48:06 -0500

    Added our strategy

--- team7.py
+++ team7.py
@@ -5,10 +5,10 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+import random
+team_name = 'Team 7' # Only 10 chars displayed.
+strategy_name = 'Betray or Random' 
+strategy_description = 'Always betray until opponent colludes then make a random choice,' 
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,16 +17,28 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    if len(my_history) ==0:
+        return "b"
+    else:
+   #     their_history = []
+   #     (my_history, their_history) = their_history[-1]
+    #    check.append(their_history)
+        if their_history[-1]=="c":
+            number = random.randint(1,2)
+            if number == 2:
+                return "b"
+            else: 
+                return "c"
+        else:
+            return "b"
+        # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
