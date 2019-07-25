60975e37bb89eca8ef44dfa6bc4effbaaad54cc2
klee074 <klee@geneva304.org>
2019-07-25 11:49:22 -0500

    Update team5.py

--- team5.py
+++ team5.py
@@ -6,17 +6,14 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'Amazing 5' # Only 10 chars displayed.
+strategy_name = 'Random'
+strategy_description = 'The program will randomly chose b or c'
+ 
+import random    
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    return random.choice(['b','c'])
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,8 +23,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
 
+    
+
+    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
